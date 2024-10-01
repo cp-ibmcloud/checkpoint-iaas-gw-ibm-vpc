b62cc5dde58e09ce7cdeb29ef48ed06147dc84ed
cp-ibmcloud <96487735+cp-ibmcloud@users.noreply.github.com>
2024-10-01 13:25:40 -0400

    Update main.tf using resources/is_instance in instance create

--- main.tf
+++ main.tf
@@ -245,6 +245,13 @@ resource "ibm_is_instance" "cp_gw_vsi" {
       }
   }
 
+network_attachments {
+    name = "external"
+    virtual_network_interface {
+      id = ibm_is_virtual_network_interface.rip_vnic_ext_vsi_gw.id
+            }
+           
+  }
   #Custom UserData
   user_data = file("user_data")
 
@@ -253,13 +260,13 @@ resource "ibm_is_instance" "cp_gw_vsi" {
     delete = "15m"
   }
 
-  resource "ibm_is_instance_network_attachment" "attach_vnic_ext_gw" {
-  instance = ibm_is_instance.cp_gw_vsi.id
-  virtual_network_interface {
-    id = ibm_is_virtual_network_interface.rip_vnic_ext_vsi_gw.id
-  }
-  name                 = "external"
-}
+  #resource "ibm_is_instance_network_attachment" "attach_vnic_ext_gw" {
+  #instance = ibm_is_instance.cp_gw_vsi.id
+  #virtual_network_interface {
+   # id = ibm_is_virtual_network_interface.rip_vnic_ext_vsi_gw.id
+  #}
+  #name                 = "external"
+#}
 
    provisioner "local-exec" {
     command = "sleep 240"
